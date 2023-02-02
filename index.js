bdd6413601cf9e2c5e395fcdcddb3fef9caf4146
Abigail <ashbella333@gmail.com>
2023-02-02 10:23:00 +0000

    added new javascriot variables

--- index.js
+++ index.js
@@ -1304,27 +1304,34 @@ init();
 // }
 // console.log(x);
 
-function redundant(a){
-    var fruit = 'mangoes';
+// function redundant(a){
+//     var fruit = 'mangoes';
 
-    function showFruit(){
-        console.log(fruit);
-    }
+//     function showFruit(){
+//         console.log(fruit);
+//     }
 
-    showFruit();
+//     showFruit();
 
-}
+// }
 
-redundant();
+// redundant();
 
 
 //Create a function that takes your position a and your friend's position b
-function diceGame(a, b){
-    const gameDice = a == b ? false : b >= a ? ((a + 6) >= b) : false;
-    console.log(gameDice);
+// function diceGame(a, b){
+//     const gameDice = a == b ? false : b >= a ? ((a + 6) >= b) : false;
+//     console.log(gameDice);
 
-}
+// }
 
-diceGame(3, 7);
+// diceGame(3, 7);
+
+let number1;
+const number2 = 44;
+let number3 = 8;
+number3 = number2;
+console(number3)
+
 
 
